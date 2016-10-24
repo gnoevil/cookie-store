34faa9b395a5bf7c00251c4dc5b5a85c6343918c
Nikko Pisciotti <t.bird.13@hotmail.com>
2016-10-24 12:08:49 -0700

    created variables

---
+++ app.js
@@ -0,0 +1,29 @@
+var first__and_pike = {
+  minCust: 23,
+  maxCust: 65,
+  avgBought: 6.3
+};
+
+var seatac = {
+  minCust: 3,
+  maxCust: 24,
+  avgBought: 1.2
+};
+
+var seattle_center = {
+  minCust: 11,
+  maxCust: 38,
+  avgBought: 3.7
+};
+
+var cap_hill = {
+  minCust: 20,
+  maxCust: 38,
+  avgBought: 2.3
+};
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgBought: 4.6
+};
