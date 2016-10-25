2107ce8b0bd98883858e4e6d94e144ba8030b070
Nikko Pisciotti <t.bird.13@hotmail.com>
2016-10-24 21:07:58 -0700

    huzzah, got my javascript to work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cookie-store
 Salmon Cookies yum
+
+
+Wow! This one has been tough for me, I've spent 6 hours trying to get my javascript to push info to my HTML - which I've finally got working! My site is completely un-styled for the moment, however that won't take long!
--- app.js
+++ app.js
@@ -1,59 +1,93 @@
-var firstAndPikeSales = [];
+
 var firstAndPike = {
+  name: 'firstAndPike',
   minCust: 23,
   maxCust: 65,
   avgBought: 6.3,
   sales: [],
-  custPerHour: function(){
-    Math.random() * 42 + 23; //this function allows my random number to be within Pat's estimates.
+  totalCookies: [],
+  custPerHour: function () {
+    var peeps = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return peeps; //this function allows my random number to be within Pat's estimates.
   }
 };
 
-var seatacSales = [];
 var seatac = {
+  name: 'seatac',
   minCust: 3,
   maxCust: 24,
   avgBought: 1.2,
   sales: [],
-  custPerHour: function() {
-    Math.random() * 21 + 3;
+  totalCookies: [],
+  custPerHour: function () {
+    var peeps = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return peeps;
   }
 };
 
-var seattleCenterSales = [];
 var seattleCenter = {
+  name: 'seattleCenter',
   minCust: 11,
   maxCust: 38,
   avgBought: 3.7,
   sales: [],
-  custPerHour: function() {
-    Math.random() * 27 + 11;
+  totalCookies: [],
+  custPerHour: function () {
+    var peeps = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return peeps;
   }
 };
 
-var capHillSales = [];
 var capHill = {
+  name: 'capHill',
   minCust: 20,
   maxCust: 38,
   avgBought: 2.3,
   sales: [],
-  custPerHour: function() {
-    Math.random() * 18 + 20;
+  totalCookies: [],
+  custPerHour: function () {
+    var peeps = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return peeps;
   }
 };
 
 var alki = {
+  name: 'alki',
   minCust: 2,
   maxCust: 16,
   avgBought: 4.6,
   sales: [],
+  totalCookies: [],
   custPerHour: function () {
-    Math.random() * 14 + 2;
+    var peeps = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+    return peeps;
   }
 };
 
-var calcCookiesBought = function(location){
-  for (hour = 0; hour <= 15; hour++);
-  var cookiesBought = location.avgBought * location.custPerHour;
-  cookiesBought = location.sales;
-};
+var hoursOpen = ['6:00am', '7:00am', '8:00am','9:00am', '10:00am', '11:00am', '12:00pm','1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm'];
+function calcCookiesAndStore(location) {
+  var newUl = document.createElement('ul');
+  newUl.className = location.name;
+  document.body.appendChild(newUl);
+  var myUl = document.getElementsByClassName(location.name)[0];
+  for (var hour = 0; hour < hoursOpen.length; hour++) {
+    var currentHour = hoursOpen[hour] + ': ';
+    var cookiesBought = Math.floor(location.avgBought * location.custPerHour());
+    location.sales.push(cookiesBought);
+    var newLi = document.createElement('li');
+    newLi.className = location;
+    newLi.innerText = currentHour + cookiesBought + 'cookies';
+    myUl.appendChild(newLi);
+  }
+}
+
+// var makeTotal = function(location) {
+//   var total = location.reduce(function(a, b) { return a + b; }, 0); //interwebs
+//   still need code to push total to html.
+// };
+
+calcCookiesAndStore(firstAndPike);
+calcCookiesAndStore(seatac);
+calcCookiesAndStore(seattleCenter);
+calcCookiesAndStore(capHill);
+calcCookiesAndStore(alki);
